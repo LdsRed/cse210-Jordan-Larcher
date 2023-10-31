849e90b289e30ba10acb48f02956bb68b662f423
Jordan Larcher <lds.larcher@gmail.com>
2023-10-30 21:35:42 -0300

    adding few changes

---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,12 @@
+public class ReflectingActivity : Activity
+{
+
+    public ReflectingActivity()
+    {
+        this._activityName = "Reflecting";
+        this._description = "This activity will help you reflect on times ...";
+        this._duration = 50;
+
+        // Set other values here that are unique to the Reflecting Activity
+    }
+}                 
