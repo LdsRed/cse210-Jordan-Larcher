48154ad2ae815a7c7f5f790785b9b658fd98c6fa
Jordan Larcher <lds.larcher@gmail.com>
2023-10-30 22:02:38 -0300

    saving changes

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -4,12 +4,10 @@ using System.Collections.Generic;
 
 class BreathingActivity: Activity
 {
-    private List<string> _prompt;
-    private List<string> _reflexion;
 
     public BreathingActivity(string activityName, string description, int duration, string endingMessage ) :base(activityName, description, duration, endingMessage)
     {
-        
+        this._activityName = "";
 
     }
 }
